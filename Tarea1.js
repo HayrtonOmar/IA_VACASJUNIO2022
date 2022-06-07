6198946ec3f0b5e430b09f9d5a10a405618eb8aa
Hayton <2172182021503@ingenieria.usac.edu.gt>
2022-06-07 09:26:33 -0600

    cambios

--- Tarea1.js
+++ Tarea1.js
@@ -2,73 +2,73 @@
 // Copyright (c) 2020 Luis Espino
 
 function reflex_agent(location, state, mode) {
-    if (mode[0]) {
-      if (location === "A") {
+  if (mode[0]) {
+    if (location === "A") {
+      mode[0] = false;
+      return "RIGHT";
+    } else {
+      if (location === "B") {
         mode[0] = false;
-        return "RIGHT";
+        return "LEFT";
+      }
+    }
+  } else {
+    if (state === "DIRTY") {
+      mode[0] = true;
+      return "CLEAN";
+    } else {
+      if (state === "CLEAN") {
+        mode[0] = true;
+        return "DIRTY";
+      }
+    }
+  }
+}
+  
+function test(states) {
+  var action, location, mode, state;
+  var mode = [false];
+
+  while (cont != 8) {
+    location = states[0];
+    state = states[0] == "A" ? states[1] : states[2];
+    action = reflex_agent(location, state, mode);
+
+    if (action === "CLEAN") {
+      if (location === "A") {
+        states[1] = "CLEAN";
       } else {
         if (location === "B") {
-          mode[0] = false;
-          return "LEFT";
+          states[2] = "CLEAN";
         }
       }
     } else {
-      if (state === "DIRTY") {
-        mode[0] = true;
-        return "CLEAN";
-      } else {
-        if (state === "CLEAN") {
-          mode[0] = true;
-          return "DIRTY";
-        }
-      }
-    }
-  }
-  
-  function test(states) {
-    var action, cont, location, mode, state;
-    var mode = [false];
-    var cont = 0;
-  
-    while (cont != 8) {
-      location = states[0];
-      state = states[0] == "A" ? states[1] : states[2];
-      action = reflex_agent(location, state, mode);
-  
-      if (action === "CLEAN") {
+      if (action === "DIRTY") {
         if (location === "A") {
-          states[1] = "CLEAN";
+          states[1] = "DIRTY";
         } else {
           if (location === "B") {
-            states[2] = "CLEAN";
+            states[2] = "DIRTY";
           }
         }
       } else {
-        if (action === "DIRTY") {
-          if (location === "A") {
-            states[1] = "DIRTY";
-          } else {
-            if (location === "B") {
-              states[2] = "DIRTY";
-            }
-          }
+        if (action === "RIGHT") {
+          states[0] = "B";
         } else {
-          if (action === "RIGHT") {
-            states[0] = "B";
-          } else {
-            if (action === "LEFT") {
-              states[0] = "A";
-            }
+          if (action === "LEFT") {
+            states[0] = "A";
           }
         }
       }
-  
-      //console.log("Location: " + location + " | Action: " + action + "  | current_status: ", states);
-      document.getElementById("log").innerHTML+="<br>Location: ".concat(location).concat(" | Action: ").concat(action).concat(" | Current_status:").concat(states);
-      cont = cont + 1;
-      setTimeout(function(){ test(states); }, 4000);
     }
+
+    document.getElementById("log").innerHTML+="<br>Location: ".concat(location).concat(" | Action: ").concat(action).concat(" | Current_status:").concat(states);
+    
+    setTimeout(function(){ test(states) }, 2000);
+    cont = cont + 1;
   }
+}
   
   var states = ["A","DIRTY","DIRTY"];
+  var cont = 0;
   test(states);
